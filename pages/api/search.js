4f52c1a25c8732c06464153ecc6a7d678329e697
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-03-30 12:58:34 +0300

    ADD: search api to search through the json file

---
+++ pages/api/search.js
@@ -0,0 +1,18 @@
+import pokemon from "../../pokemon.json";
+
+const searchFunc =  (req, res) => {
+  const filter = req.query.q ? new RegExp(req.query.q, "i") : /.*/;
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+
+  res.end(
+    JSON.stringify(
+      pokemon
+        .filter(({ name: { english } }) => english.match(filter))
+        .slice(0, 10)
+    )
+  );
+};
+
+export default searchFunc;
