65b21d2058a8e0055281d1c8b4c32ed96b050ecc
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-03-30 12:57:49 +0300

    ADD: configurations for mantine

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { useState } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App(props) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState("light");
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  return (
+    <>
+      <Head>
+        <title>Pokemon</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            fontFamily: "verdana, sans-serif",
+            fontFamilyMonospace: "Monaco, Courier, monospace",
+            colorScheme,
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
-
-export default MyApp
